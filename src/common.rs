71a4fb4c8ff89cb80e1df9be1ed9f32c753ceba2
pyrrhica <pyrrhica@gmail.com>
2025-06-22 07:51:13 +0800

    修改伺服器API連線位置

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rust.gtrotech.com:21114".to_owned()
+    "http://ageart.synology.me:21114".to_owned()
 }
 
 #[inline]
